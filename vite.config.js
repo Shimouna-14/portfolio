40176b61923fb5b731ca193dad5daa7ccd12bb72
Shimouna-14 <chimene.ndeby@gmail.com>
2024-04-09 13:09:58 +0200

    Fix api

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
-  ]
+  plugins: [react()],
 })
